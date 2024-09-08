7de1ffb2de7ef9adfebfb77f5f519f476f3af5d9
danicode <danielanzawa@gmail.com>
2024-09-08 01:29:54 -0300

    Agregado de link a mi repo en el footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
 
   return (
     <footer id="footer">
-      <a href="https://github.com/danicode" target="_blank" id="repo"><img src="https://github.githubassets.com/images/icons/emoji/octocat.png" alt="Octocat" width="50" height="50"/>¡Visita mi repo!</a>
+      <a href="https://github.com/danicode/adivinanzas" target="_blank" id="repo"><img src="https://github.githubassets.com/images/icons/emoji/octocat.png" alt="Octocat" width="50" height="50"/>¡Visita mi repo!</a>
     </footer>
   );
 }
